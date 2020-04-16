c27fee73a1e1124e7f7378282febbea9afa6feae
spaghettiwews <spaghettiwews@outlook.com>
2020-04-16 09:01:30 +0200

    icommit yokuqala

---
+++ app/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <>
+      <aside>Main menu</aside>
+      <main>testing</main>
+      <aside>Next up</aside>
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ app/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import routes from '../constants/routes.json';
+// import styles from './Home.css';
+import Dashboard from './Dashboard';
+
+export default function Home() {
+  return <Dashboard />;
+}
